539d716a02df45e6e1b77364bee3401d731355d4
MedNidhalSamet <nsamet21@gmail.com>
2024-06-13 22:22:56 +0200

    Adjust website

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <p>
+      © 2024 Mohamed Nidhal Samet . All rights reserved.
+    </p>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,60 @@
+const header = {
+  homepage: 'https://github.com/SametNidhal',
+  title: 'MNs',
+}
+
+const about = {
+  name: 'Mohamed Nidhal Samet',
+  role: 'Software Engineer',
+  description:
+    'Welcome! 👋 With a background in Software Engineering and extensive experience in Guidewire development and release management, I specialize in delivering innovative solutions and managing seamless software releases. 🚀',
+  resume: 'https://drive.usercontent.google.com/download?id=1z7pRmAP0jJeHBexKvvtyeXFUEntkBAhc&export=download&authuser=5',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/nidhal-samet/',
+    github: 'https://github.com/SametNidhal',
+  },
+}
+
+const projects = [
+  {
+    name: 'Release Manager',
+    description: 'Working with CICD tools like Bitbucket, GIT, TeamCity, DataDog and GCC (Guidewire Cloud Console) Managing, planning, scheduling and controlling software build through different stages and environments.',
+    stack: 'Jan 2024 - Present',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Logo-pwc.png',
+  },
+  {
+    name: 'Guidewire Developer',
+    description: 'Translate business requirements into technical specifications for the BillingCenter application. Conduct unit tests to ensure the functionality and reliability of developed features. Additionally, I collaborated closely with development, testing, business analyst, and Scrum master teams to ensure the timely delivery of features.',
+    stack: 'May 2022 - Dec 2023',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Logo-pwc.png',
+  },
+  {
+    name: 'Full Stack Developer',
+    description: ' Developed a web application for managing purchase orders. I worked on the front-end using Angular, creating and maintaining user interfaces in collaboration with UI/UX designers. I integrated APIs and external services for data manipulation. On the back-end, I utilized Spring Boot to develop RESTful APIs, design database schemas, and implement security measures with JWT, ensuring the robust functionality of back-end services.',
+    stack: 'April 2021 - Sept 2021',
+    imageUrl: 'https://hitsolutions.tech/wp-content/themes/hits/images/Logo_HITS_01.png',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'Java',
+  'AZURE',
+  'UX Design',
+  'Git',
+  'CI/CD',
+  'DataDog',
+  'TeamCity',
+  'Guidewire',
+  'Confluence',
+  'Jira',
+  'Angular',
+  'Sprint Boot',
+]
+
+const contact = {
+  email: ' ',
+}
+
+export { header, about, projects, skills, contact }
